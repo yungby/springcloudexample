1bb20927de94a2c1c51dde1a21d615a4642055ce
yungby <yungbyzy@163.com>
2018-11-02 15:36:57 +0800

    init

---
+++ admin-service/src/main/java/com/andy/cloud/admin/AdminApplication.java
@@ -0,0 +1,28 @@
+package com.andy.cloud.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SpringBootApplication
+@EnableAdminServer
+@EnableEurekaClient
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+
+    @EnableWebSecurity
+    public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable();
+            super.configure(http);
+        }
+    }
+}
