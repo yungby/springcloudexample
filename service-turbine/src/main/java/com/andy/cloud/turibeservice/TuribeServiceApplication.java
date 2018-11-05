deba3479ff8a65534390cd9dad4fe76e939c7e2b
yungby <yungbyzy@163.com>
2018-11-05 18:07:04 +0800

    init

---
+++ service-turbine/src/main/java/com/andy/cloud/turibeservice/TuribeServiceApplication.java
@@ -0,0 +1,12 @@
+package com.andy.cloud.turibeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuribeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuribeServiceApplication.class, args);
+    }
+}
